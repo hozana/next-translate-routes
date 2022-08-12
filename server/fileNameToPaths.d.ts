a3a23ec666de270de52dbc446aa7a5429dcdaceb
cvolant <corentinvolant@ntymail.com>
2022-08-12 11:30:27 +0200

    Fix gitgnore

--- server/fileNameToPaths.d.ts
+++
@@ -1,2 +0,0 @@
-/** Transform Next file-system synthax to path-to-regexp synthax */
-export declare const fileNameToPath: (fileName: string) => string
--- shared/ntrData.d.ts
+++
@@ -1,4 +0,0 @@
-import type { TNtrData } from '../types'
-
-export declare const setNtrData: (ntrData: TNtrData) => void
-export declare const getNtrData: () => TNtrData
