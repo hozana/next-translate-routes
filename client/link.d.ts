0c0ce683883971daf37f50a215dc8bb5c1c6f7cc
cvolant <corentinvolant@ntymail.com>
2022-08-12 11:30:27 +0200

    Fix gitgnore

--- client/link.d.ts
+++
@@ -1,8 +0,0 @@
-import { LinkProps } from 'next/link'
-import React from 'react'
-
-/**
- * Link component that handle route translations
- */
-export declare const Link: React.FC<React.PropsWithChildren<LinkProps>>
-export default Link
