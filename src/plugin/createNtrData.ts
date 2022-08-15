cc591c0cbde0c9df86da0f11eda34ac984915797
cvolant <corentinvolant@ntymail.com>
2022-08-15 12:40:14 +0200

    Remove Next defaultConfig import

--- src/plugin/createNtrData.ts
+++ src/plugin/createNtrData.ts
@@ -1,17 +1,15 @@
-import { defaultConfig } from 'next/dist/server/config-shared'
+import type { NextConfig } from 'next/dist/server/config-shared'
 
 import type { NextConfigWithNTR, TNtrData } from '../types'
 import { getPagesPath } from './getPagesPath'
 import { parsePages } from './parsePages'
 
-export const createNtrData = (
-  {
-    pageExtensions = defaultConfig.pageExtensions as string[],
+export const createNtrData = (nextConfig: NextConfig, customPagesPath?: string): TNtrData => {
+  const {
+    pageExtensions = ['js', 'ts', 'jsx', 'tsx'],
     i18n: { defaultLocale, locales = [] },
     translateRoutes: { debug, routesDataFileName, routesTree: customRoutesTree, pagesDirectory } = {},
-  }: NextConfigWithNTR,
-  customPagesPath?: string,
-): TNtrData => {
+  } = nextConfig as NextConfigWithNTR
   const pagesPath = customPagesPath || getPagesPath(pagesDirectory)
 
   const routesTree = customRoutesTree || parsePages({ directoryPath: pagesPath, pageExtensions, routesDataFileName })
