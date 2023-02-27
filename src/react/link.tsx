9ed13a63813141b41e759cab908883d089e99592
Sander Korf <sander@maia.insure>
2023-02-27 16:51:21 +0100

    Updates type of Link component

--- src/react/link.tsx
+++ src/react/link.tsx
@@ -1,13 +1,18 @@
 import NextLink, { LinkProps } from 'next/link'
 import { useRouter as useNextRouter } from 'next/router'
-import React from 'react'
+import React, { type FC, type AnchorHTMLAttributes, type ReactNode } from 'react'
 
 import { translatePushReplaceArgs } from './translatePushReplaceArgs'
 
 /**
  * Link component that handle route translations
  */
-export const Link: React.FC<React.PropsWithChildren<LinkProps>> = ({ href, as, locale, ...props }) => {
+export const Link: FC<
+  Omit<AnchorHTMLAttributes<HTMLAnchorElement>, keyof LinkProps> &
+    LinkProps & {
+      children?: ReactNode
+    } & React.RefAttributes<HTMLAnchorElement>
+> = ({ href, as, locale, ...props }) => {
   const router = useNextRouter()
   const translatedArgs = translatePushReplaceArgs({ router, url: href, as, locale })
 
