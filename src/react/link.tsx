ef9c55ca48091d35d78aba9d89c8138f9dda6a0e
cvolant <corentinvolant@ntymail.com>
2023-04-25 15:10:07 +0200

    Forward ref in Link #Fix 67

--- src/react/link.tsx
+++ src/react/link.tsx
@@ -1,22 +1,25 @@
 import NextLink, { LinkProps } from 'next/link'
 import { useRouter as useNextRouter } from 'next/router'
-import React, { type FC, type AnchorHTMLAttributes, type ReactNode } from 'react'
+import React, { type AnchorHTMLAttributes, type ReactNode } from 'react'
 
 import { translatePushReplaceArgs } from './translatePushReplaceArgs'
 
 /**
  * Link component that handle route translations
  */
-export const Link: FC<
+export const Link = React.forwardRef<
+  HTMLAnchorElement,
   Omit<AnchorHTMLAttributes<HTMLAnchorElement>, keyof LinkProps> &
     LinkProps & {
       children?: ReactNode
     } & React.RefAttributes<HTMLAnchorElement>
-> = ({ href, as, locale, ...props }) => {
+>(({ href, as, locale, ...props }, ref) => {
   const router = useNextRouter()
   const translatedArgs = translatePushReplaceArgs({ router, url: href, as, locale })
 
-  return <NextLink href={translatedArgs.url} as={translatedArgs.as} locale={translatedArgs.locale} {...props} />
-}
+  return (
+    <NextLink ref={ref} href={translatedArgs.url} as={translatedArgs.as} locale={translatedArgs.locale} {...props} />
+  )
+})
 
 export default Link
