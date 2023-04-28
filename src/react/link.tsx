b068ef9c4d5660aad284418bc9366f4fe1704fcf
cvolant <corentinvolant@ntymail.com>
2023-04-28 18:03:57 +0200

    Update next to 13.3.1 and fix link type

--- src/react/link.tsx
+++ src/react/link.tsx
@@ -1,25 +1,21 @@
-import NextLink, { LinkProps } from 'next/link'
+import NextLink, { type LinkProps } from 'next/link'
 import { useRouter as useNextRouter } from 'next/router'
-import React, { type AnchorHTMLAttributes, type ReactNode } from 'react'
+import React from 'react'
 
 import { translatePushReplaceArgs } from './translatePushReplaceArgs'
 
 /**
  * Link component that handle route translations
  */
-export const Link = React.forwardRef<
-  HTMLAnchorElement,
-  Omit<AnchorHTMLAttributes<HTMLAnchorElement>, keyof LinkProps> &
-    LinkProps & {
-      children?: ReactNode
-    } & React.RefAttributes<HTMLAnchorElement>
->(({ href, as, locale, ...props }, ref) => {
-  const router = useNextRouter()
-  const translatedArgs = translatePushReplaceArgs({ router, url: href, as, locale })
+export const Link: typeof NextLink = React.forwardRef<HTMLAnchorElement, LinkProps>(
+  ({ href, as, locale, ...props }, ref) => {
+    const router = useNextRouter()
+    const translatedArgs = translatePushReplaceArgs({ router, url: href, as, locale })
 
-  return (
-    <NextLink ref={ref} href={translatedArgs.url} as={translatedArgs.as} locale={translatedArgs.locale} {...props} />
-  )
-})
+    return (
+      <NextLink ref={ref} href={translatedArgs.url} as={translatedArgs.as} locale={translatedArgs.locale} {...props} />
+    )
+  },
+)
 
 export default Link
